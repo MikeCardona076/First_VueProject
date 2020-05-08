a834cddb94286bf4b3f8a0f77dcde3ba89e1077d
mikecardona076 <mikecardona076@gmail.com>
2020-05-07 20:06:14 -0700

    1.0

---
+++ FirstClass/helloworld.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'People!',
+      names:[
+        { name:'Esmeralda', age:23, profession:'Doctor' },
+        { name:'Victoria',age:43, profession:'Programmer' },
+        { name:'Julieta',age:63, profession:'Taxi driver' }] 
+    }
+     
+  })
